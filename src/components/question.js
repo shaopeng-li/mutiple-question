bc1563cc8e7f18bb7083f35d3b884a8cdc5b5932
LI <shaopeng.li@monsanto.com>
2018-10-12 14:00:48 -0500

    style selected answer

--- src/components/question.js
+++ src/components/question.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 const question = (props) => {
+    let yesStyle = null;
+    let noStyle = null;
+
+    const style = {
+        backgroundColor: 'gray',
+        color: 'white',
+        border: '1px solid bule'
+    }
+
+    if (props.details.selected === 'yes') {
+        yesStyle = style;
+    }
+    if (props.details.selected === 'no') {
+        noStyle = style;
+    }
+
     return (
         <div>
             <h2>{props.details.title}</h2>
             <p>{props.details.question}</p>
-            <button onClick={() => props.chooseAnswer('yes')}>Yes</button>
-            <button onClick={() => props.chooseAnswer('no')}>No</button>
+            <button style={yesStyle} onClick={() => props.chooseAnswer('yes')}>Yes</button>
+            <button style={noStyle} onClick={() => props.chooseAnswer('no')}>No</button>
         </div>
     );
 }
