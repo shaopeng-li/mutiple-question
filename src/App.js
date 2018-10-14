ddf9a05b1501aab9d834eefc1f33fef39f784e7d
ShaopengLi <811131196@qq.com>
2018-10-14 00:09:21 -0500

    pass user name from child component to parent

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import QuestionSet from './containers/QuestionSet/questionSet';
 import LandingPage from './components/Landing/LandingPage';
 
 class App extends Component {
-  startAnswerQuestion = (input) => {
-    console.log(input);
+  state = {
+    userName: ''
+  }
+
+  startAnswerQuestion = () => {
     this.props.history.push('/assignment');
+  }
+
+  nameChangeHandler = (name) => {
+    this.setState({userName: name});
   }
 
   render() {
     return (
       <div className="App">
         <Switch>
-          <Route path='/' exact render={() => (<LandingPage start={this.startAnswerQuestion} />)} />
-          <Route path='/assignment' component={QuestionSet} />
+          <Route path='/' exact render={() => (<LandingPage start={this.startAnswerQuestion} nameChangeHandler={this.nameChangeHandler} />)} />
+          <Route path='/assignment' render={() => <QuestionSet userName={this.state.userName} />} />
         </Switch>
       </div>
     );
--- src/components/Landing/LandingPage.js
+++ src/components/Landing/LandingPage.js
@@ -4,15 +4,13 @@ import classes from './landing.module.css';
 import { Button } from 'react-bootstrap';
 
 const LandingPage = (props) => {
-    let textInput = React.createRef();
-
     return (
         <div className={classes.landingWraper}>
             <h1>Welcome to mutiple question app!</h1>
             <p>Please click below button for anwser question, you may get your point at last page!</p>
             <form>
-                <input type="text" required ref={textInput}></input>
-                <Button onClick={(textInput) => props.start(textInput)}>Click to start</Button>
+                <input type="text" onChange={(event) => props.nameChangeHandler(event.target.value)} required></input>
+                <Button onClick={props.start}>Click to start</Button>
             </form>
             {/* <button><Link to='/assignment'>Click to start</Link></button> */}
         </div>
